ec80ccd810ca034e1bfcb9ca6834185a44c946fd
ngocthachnguyen <ngocthach.nguyen1401@gmail.com>
2025-09-26 11:01:23 +0700

    update format

--- shopapp-backend/src/main/java/com/project/shopapp/repositories/ProductRepository.java
+++ shopapp-backend/src/main/java/com/project/shopapp/repositories/ProductRepository.java
@@ -15,12 +15,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByName(String name);
     Page<Product> findAll(Pageable pageable);//phân trang
     List<Product> findByCategory(Category category);
+
     @Query("SELECT p FROM Product p WHERE " +
             "(:categoryId IS NULL OR :categoryId = 0 OR p.category.id = :categoryId) " +
             "AND (:keyword IS NULL OR :keyword = '' OR p.name LIKE %:keyword% OR p.description LIKE %:keyword%)")
-    Page<Product> searchProducts
-            (@Param("categoryId") Long categoryId,
+    Page<Product> searchProducts(@Param("categoryId") Long categoryId,
              @Param("keyword") String keyword, Pageable pageable);
+
     @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImages WHERE p.id = :productId")
     Optional<Product> getDetailProduct(@Param("productId") Long productId);
 
